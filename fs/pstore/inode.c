28f182d556d2d0c2e8ab5ca15f158c131ce0f5bc
mayuri <94937447+mayuri-hikari@users.noreply.github.com>
2024-10-11 09:31:50 +0000

    Revert "pstore: import xiaomi last-kmsg changes"
    This reverts commit 9840cb143649e695783d288bb43f1fe56f1ea8a8.

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -38,9 +38,6 @@
 #include <linux/uaccess.h>
 
 #include "internal.h"
-#ifdef CONFIG_PSTORE_LAST_KMSG
-#include <linux/proc_fs.h>
-#endif
 
 #define	PSTORE_NAMELEN	64
 
@@ -300,22 +297,6 @@ bool pstore_is_mounted(void)
 	return pstore_sb != NULL;
 }
 
-#ifdef CONFIG_PSTORE_LAST_KMSG
-static char *console_buffer;
-static ssize_t console_bufsize;
-static ssize_t last_kmsg_read(struct file *file, char __user *buf,
-		size_t len, loff_t *offset)
-{
-	return simple_read_from_buffer(buf, len, offset,
-			console_buffer, console_bufsize);
-}
-static const struct file_operations last_kmsg_fops = {
-	.owner          = THIS_MODULE,
-	.read           = last_kmsg_read,
-	.llseek         = default_llseek,
-};
-#endif
-
 /*
  * Make a regular file in the root directory of our file system.
  * Load it up with "size" bytes of data from "buf".
@@ -422,14 +403,7 @@ int pstore_mkfile(struct dentry *root, struct pstore_record *record)
 	list_add(&private->list, &allpstore);
 	spin_unlock_irqrestore(&allpstore_lock, flags);
 
-	#ifdef CONFIG_PSTORE_LAST_KMSG
-	if (type == PSTORE_TYPE_CONSOLE) {
-		console_buffer = private->data;
-		console_bufsize = size;
-
-		return 0;
-	}
-#endif
+	return 0;
 
 fail_private:
 	free_pstore_private(private);
@@ -515,10 +489,6 @@ int __init pstore_init_fs(void)
 {
 	int err;
 
-#ifdef CONFIG_PSTORE_LAST_KMSG
-	struct proc_dir_entry *last_kmsg_entry = NULL;
-#endif
-
 	/* Create a convenient mount point for people to access pstore */
 	err = sysfs_create_mount_point(fs_kobj, "pstore");
 	if (err)
@@ -527,15 +497,6 @@ int __init pstore_init_fs(void)
 	err = register_filesystem(&pstore_fs_type);
 	if (err < 0)
 		sysfs_remove_mount_point(fs_kobj, "pstore");
-
-	#ifdef CONFIG_PSTORE_LAST_KMSG
-	last_kmsg_entry = proc_create_data("last_kmsg", S_IFREG | S_IRUGO,
-			NULL, &last_kmsg_fops, NULL);
-	if (!last_kmsg_entry) {
-		pr_err("Failed to create last_kmsg\n");
-		goto out;
-	}
-#endif
 
 out:
 	return err;
